ff1ba9c73029950a93ba245a88ff0aaf99a67d35
Elias Santos Martins <eliasgjt@gmail.com>
2025-08-16 21:15:08 -0300

    feat(#5): adiciona load das imagens.

--- src/src/utils/imageutils.py
+++ src/src/utils/imageutils.py
@@ -1,2 +1,31 @@
-def ola():
-    print("ola")
+import cv2
+import os
+import sys
+from pathlib import Path
+
+
+try:
+# Works in scripts
+    current_file = Path(__file__).resolve()
+except NameError:
+    # Fallback for interactive sessions like Jupyter
+    current_file = Path(sys.argv[0]).resolve() if sys.argv[0] else Path.cwd()
+
+current_dir = current_file.parent
+
+BASE_DATA_PATH = current_dir.parent.parent.parent / "data"
+
+
+def load_raw_images(panorama: str) -> dict[str, cv2.Mat]:
+    path = BASE_DATA_PATH / "raw" / panorama
+    images: dict[str, cv2.Mat] = {}
+    
+    for file in sorted(path.glob("*")):  # iterate over files
+        if file.suffix.lower() in [".jpg", ".jpeg", ".png"]:
+            img = cv2.imread(str(file))
+            if img is not None:
+                # Use filename without extension as key
+                key = file.stem  
+                images[key] = img
+    
+    return images
