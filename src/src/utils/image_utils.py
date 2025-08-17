e3fc84986a62bea92738279a7cd660353ffe9266
Elias Santos Martins <eliasgjt@gmail.com>
2025-08-16 22:34:17 -0300

    feat(#5): Add save image method.

--- src/src/utils/image_utils.py
+++ src/src/utils/image_utils.py
@@ -20,6 +20,16 @@ BASE_DATA_PATH = current_dir.parent.parent.parent / "data"
 
 
 def load_raw_images(panorama: str) -> dict[str, cv2.Mat]:
+    """
+    Load all images from a raw panorama folder into a dictionary.
+
+    Args:
+        panorama (str): Name of the panorama folder under "raw".
+
+    Returns:
+        Dict[str, cv2.Mat]: A dictionary mapping filename (without extension)
+                             to the loaded OpenCV image.
+    """
     path = BASE_DATA_PATH / "raw" / panorama
     images: dict[str, cv2.Mat] = {}
     
@@ -32,3 +42,25 @@ def load_raw_images(panorama: str) -> dict[str, cv2.Mat]:
                 images[key] = img
     
     return images
+
+
+def save_image(img: cv2.Mat, filename : str, path: str = "interim", img_format : str = ".jpg") -> bool:
+    """
+    Save an OpenCV image to disk.
+
+    Args:
+        img (cv2.Mat): The image to save.
+        filename (str): Name of the output file (without extension).
+        path (str, optional): Subfolder under BASE_DATA_PATH to save the image. Default is "interim".
+        format (str, optional): Image format/extension (e.g., 'jpg', 'png'). Default is 'jpg'.
+
+    Returns:
+        bool: True if the image was saved successfully, False otherwise.
+    """
+    # Ensure format does not have a leading dot
+    img_format = img_format.lstrip(".")
+    output_path = BASE_DATA_PATH / path / f"{filename}.{img_format}"
+    
+    return cv2.imwrite(output_path, img)
+    
+    
