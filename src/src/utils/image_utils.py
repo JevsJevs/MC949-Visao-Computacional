fe91fc4880a52b4bfc2a97b2623582b818b9872d
VictorManoelRG <victormanoelrg@gmail.com>
2025-08-24 10:19:34 -0300

    Corrigindo comentarios

--- src/src/utils/image_utils.py
+++ src/src/utils/image_utils.py
@@ -65,7 +65,8 @@ def save_image(img: cv2.Mat, filename : str, path: str = "interim", img_format :
     return cv2.imwrite(output_path, img)
 
 
-def draw_pairing_lines(img1, img2, alg:str ,filename : str, path: str = "interim", img_format : str = ".jpg"):
+def draw_pairing_lines(img1: cv2.Mat, img2: cv2.Mat, alg: str, filename: str,
+                       path: str = "interim", img_format: str = ".jpg"):
 
     algs = {
         "SIFT": feature_extraction.SIFT,
@@ -78,18 +79,36 @@ def draw_pairing_lines(img1, img2, alg:str ,filename : str, path: str = "interim
     kp1, des1 = func(img1)
     kp2, des2 = func(img2)
 
-    bf = cv2.BFMatcher() 
-
+    bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
 
-    img_matches = cv2.drawMatches(img1, kp1, img2, kp2, david_loew_ratio_test(matches), None, flags=2)
+    # Aplica Lowe ratio test
+    good_matches = david_loew_ratio_test(matches)
+
+    # Desenha as linhas
+    img_matches = cv2.drawMatches(img1, kp1, img2, kp2, good_matches, None, flags=2)
+
+    # --- Marcar início e fim ---
+    h1, w1 = img1.shape[:2]
+
+    for m in good_matches:
+        pt1 = tuple(map(int, kp1[m.queryIdx].pt))  # ponto na img1
+        pt2 = tuple(map(int, kp2[m.trainIdx].pt))  # ponto na img2
+        pt2 = (int(pt2[0] + w1), int(pt2[1]))      # compensar deslocamento da concatenação
+
+        # "X" no ponto inicial (img1)
+        cv2.drawMarker(img_matches, pt1, (0, 255, 0), markerType=cv2.MARKER_TILTED_CROSS,
+                       markerSize=12, thickness=2, line_type=cv2.LINE_AA)
+
+        # "O" no ponto final (img2)
+        cv2.circle(img_matches, pt2, 6, (0, 0, 255), 2, lineType=cv2.LINE_AA)
 
     save_image(img_matches, filename, path, img_format)
 
     return img_matches
 
 
-def david_loew_ratio_test(matches, ratio=0.75):
+def david_loew_ratio_test(matches: cv2.DMatch, ratio=0.75):
     """
     Receives the matches returned by knnMatch and applies the ratio test.
     
