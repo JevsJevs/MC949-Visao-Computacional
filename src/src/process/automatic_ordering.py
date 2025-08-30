f55c0c558f368b7395b42c4673366f407cc59268
Elias Santos Martins <eliasgjt@gmail.com>
2025-08-29 21:32:50 -0300

    feat(#27): adiciona heuristica do caminho hamiltoniano e deteccao de inversao.

--- src/src/process/automatic_ordering.py
+++ src/src/process/automatic_ordering.py
@@ -9,7 +9,7 @@ def hamiltonian_path_brute_force(G: nx.Graph) -> list[int]:
     n = G.number_of_nodes()
     best_path: list[int] | None = None
     
-    min_cost = float('inf')  # safer than a hard-coded large number
+    max_cost = 0
 
     # Try all permutations of nodes
     for perm in itertools.permutations(G.nodes):
@@ -27,19 +27,52 @@ def hamiltonian_path_brute_force(G: nx.Graph) -> list[int]:
                 break
 
         # Update best path if valid and cheaper
-        if valid and cost < min_cost:
-            min_cost = cost
+        if valid and cost > max_cost:
+            max_cost = cost
             best_path = path
 
     return best_path
 
 
 def hamiltonian_path_heuristic(G: nx.Graph) -> list[int]:
-    # TODO
-    return None
+    n = G.number_of_nodes()
+    
+    # começa com a aresta de menor peso
+    u, v, w = max(G.edges(data="weight"), key=lambda x: x[2])
+    path = [u, v]  # inicia o caminho com os dois vértices da aresta mínima
+    
+    # enquanto não tiver todos os nós
+    while len(path) < n:
+        candidates = []
+        
+        # tenta expandir pela ponta esquerda
+        for neighbor, data in G[path[0]].items():
+            if neighbor not in path:
+                candidates.append((data["weight"], neighbor, "left"))
+        
+        # tenta expandir pela ponta direita
+        for neighbor, data in G[path[-1]].items():
+            if neighbor not in path:
+                candidates.append((data["weight"], neighbor, "right"))
+        
+        if not candidates:
+            break  # não conseguiu expandir mais
+        
+        # pega a aresta candidata de menor custo
+        w, node, side = max(candidates, key=lambda x: x[0])
+        
+        if side == "left":
+            path.insert(0, node)
+        else:
+            path.append(node)
+    
+    return path
 
-def build_match_graph(kp_descs: list[np.ndarray], n: int) -> nx.Graph:
+
+def build_match_graph(kp_descs: list[np.ndarray], n: int) -> tuple[nx.Graph, dict[int, list[cv2.DMatch]]]:
     match_graph = {x: [] for x in range(n)}
+    
+    matches_per_image = {x: [] for x in range(n)}
 
     for i in range(n):
         for j in range(n):
@@ -59,22 +92,69 @@ def build_match_graph(kp_descs: list[np.ndarray], n: int) -> nx.Graph:
                     
             match_graph[i].append([j, len(good_matches)])
             
+            matches_per_image[i].extend(good_matches)
+            
     G = nx.Graph()
 
     # Add edges with weights
     for node, neighbors in match_graph.items():
         for neighbor, weight in neighbors:
-            G.add_edge(node, neighbor, weight=-weight)
+            G.add_edge(node, neighbor, weight=weight)
             
-    return G
+    return G, matches_per_image
+
+
+def is_inverted(path: list[int], 
+                images: list[cv2.Mat], 
+                matches_per_image: dict[int, list[cv2.DMatch]], 
+                kp_descs: tuple[list[cv2.KeyPoint], np.ndarray]):
+    
+    """Calcula o quão a esquerda estão os keypoints que deram match de cada uma das pontas do caminho
+    para saber se está invertido"""
+    
+    begin_path = path[0]
+
+    keypoints_match_begin = []
+    for match in matches_per_image[begin_path]:
+        keypoints_match_begin.append(kp_descs[begin_path][0][match.queryIdx])
+        
+    end_path = path[-1]
+
+    keypoints_match_end = []
+    for match in matches_per_image[end_path]:
+        keypoints_match_end.append(kp_descs[end_path][0][match.queryIdx])
+    
+    width_image_begin = images[begin_path].shape[1]
+
+    left_score_begin = 0
+
+    for keypoint in keypoints_match_begin:
+        left_score_begin += 1 - (keypoint.pt[0] / width_image_begin) # y coordinate
+    
+    
+    width_image_end = images[end_path].shape[1]
+
+    left_score_end = 0
+
+    for keypoint in keypoints_match_end:
+        left_score_end += 1 - (keypoint.pt[0] / width_image_end) # y coordinate
+        
+    return left_score_begin > left_score_end
+    
 
 
 def find_order(images: list[cv2.Mat]) -> list[int]:
     kp_descs = [feature_extraction.SIFT(img, nfeatures=1000) for img in images]
     
-    G = build_match_graph(kp_descs, len(images))
+    G, matches_per_image = build_match_graph(kp_descs, len(images))
     
     if len(images) < 10:
-        return hamiltonian_path_brute_force(G)
+        path = hamiltonian_path_brute_force(G)
     else:
-        return hamiltonian_path_heuristic(G)
+        path = hamiltonian_path_heuristic(G)
+        
+    # path can be inverted
+    if (is_inverted(path, images, matches_per_image, kp_descs)):
+        return path[::-1]
+    
+    return path
