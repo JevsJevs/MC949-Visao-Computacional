a5db811305c0d226a63104c153ec7448ae4f6147
Elias Santos Martins <eliasgjt@gmail.com>
2025-09-24 23:27:09 -0300

    feat(#51): upload datasets no kaggle

--- data/T1/raw/FotosICMural/README.txt
+++
@@ -1,4 +0,0 @@
-Noção aproximada da organização das fotos no panorama. Considerando seus nomes/numeração
-
-|1  |2  |3  |4  |5  |6  |7  |8  |9  |
-|17 |16 |15 |14 |13 |12 |11 |10 |
---
+++ src/canon/download_data.py
@@ -0,0 +1,57 @@
+import kagglehub
+import os
+import shutil
+from pathlib import Path
+
+from canon.utils import image_utils
+
+def download_kaggle_dataset(dataset_slug, project_name):
+    """
+    Downloads a Kaggle dataset to the specified local path.
+
+    Args:
+        dataset_slug (str): The unique identifier for the dataset 
+                            (e.g., 'titanic' for 'titanic-dataset' or 
+                            'datasnaek/youtube-new').
+        path (str): The directory where the dataset should be saved. 
+                    Default is the current directory.
+    """
+    # Create the directory if it doesn't exist
+    # path = image_utils.BASE_DATA_PATH / project_name
+    # if not os.path.exists(path):
+    #     os.makedirs(path)
+    #     print(f"Created directory: {path}")
+
+    kaggle_download_path = kagglehub.dataset_download(dataset_slug)
+    
+    kaggle_download_path = Path(kaggle_download_path)
+    
+    dest_path = image_utils.BASE_DATA_PATH
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
+        print(f"Created directory: {dest_path}")
+    
+    print(kaggle_download_path / project_name)
+    print(dest_path)
+    
+    shutil.move(kaggle_download_path / "T1", dest_path)
+    
+    # 1
+    os.rmdir(kaggle_download_path)
+    # versions
+    os.rmdir(kaggle_download_path.parent)
+    
+    
+        
+        
+if __name__ == "__main__":
+    datasets = {
+        "T1": "eliassantosmartins/mc949-t1",
+        "T2":  "eliassantosmartins/mc929-t2"
+    }
+    
+    for project, dataset in datasets.items():
+        try:
+            download_kaggle_dataset(dataset, project)
+        except Exception as e:
+            print(e)
